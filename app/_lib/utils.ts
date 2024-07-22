63e409c4931d06eb159d5d8f33a66431d531404b
Req <snogedirl@gmail.com>
2024-07-23 00:41:28 +0200

    add basic layout for notes

--- app/_lib/utils.ts
+++ app/_lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatDate(date: string) {
+  return new Date(date).toLocaleDateString(undefined, {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+}
--- app/account/notepad/page.tsx
+++ app/account/notepad/page.tsx
@@ -1,3 +1,5 @@
+import { mockedData } from "@/app/_lib/mock-data";
+import { formatDate } from "@/app/_lib/utils";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -6,12 +8,25 @@ export const metadata: Metadata = {
 };
 
 export default function NotepadPage() {
+  const data = [...mockedData, ...mockedData, ...mockedData];
   return (
-    <main className="flex-1 px-8 py-8">
-      {" "}
-      {Array.from({ length: 200 }, (_, index) => (
-        <div key={index}>meow</div>
-      ))}
+    <main className="flex-1 overflow-hidden px-6 py-4 md:px-10">
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-10">
+        {data.map((note, id) => (
+          <div
+            key={id}
+            className="flex cursor-pointer flex-col gap-4 rounded-lg border-2 border-secondary/40 bg-bg1 px-6 py-4 shadow-md transition-[border] duration-500 hover:border-primary"
+          >
+            <div className="grid gap-1">
+              <h2 className="text-xl font-semibold text-primary">
+                {note.title}
+              </h2>
+              <p className="text-secondary">{formatDate(note.created_at)}</p>
+            </div>
+            <p className="text-primary">{note.body}</p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
