731a2fc3d7f8c1a9d6aeb91d84a37d0e906cf107
Req <snogedirl@gmail.com>
2024-07-23 00:58:19 +0200

    small changes to styling of notes

--- app/account/notepad/page.tsx
+++ app/account/notepad/page.tsx
@@ -10,18 +10,18 @@ export const metadata: Metadata = {
 export default function NotepadPage() {
   const data = [...mockedData, ...mockedData, ...mockedData];
   return (
-    <main className="flex-1 overflow-hidden px-6 py-4 md:px-10">
+    <main className="flex-1 px-6 py-4 md:px-10">
       <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-10">
         {data.map((note, id) => (
           <div
             key={id}
-            className="flex cursor-pointer flex-col gap-4 rounded-lg border-2 border-secondary/40 bg-bg1 px-6 py-4 shadow-md transition-[border] duration-500 hover:border-primary"
+            className="flex cursor-pointer flex-col gap-10 rounded-lg border-2 border-secondary/40 bg-bg1 px-6 py-4 shadow-md transition-[border] duration-500 hover:border-primary"
           >
             <div className="grid gap-1">
-              <h2 className="text-xl font-semibold text-primary">
-                {note.title}
-              </h2>
-              <p className="text-secondary">{formatDate(note.created_at)}</p>
+              <h2 className="text-xl font-bold text-primary">{note.title}</h2>
+              <p className="text-sm font-medium text-secondary">
+                {formatDate(note.created_at)}
+              </p>
             </div>
             <p className="text-primary">{note.body}</p>
           </div>
