1781b0f8404d9b4bbdba97a1e34e284a0facaf50
Req <snogedirl@gmail.com>
2024-07-30 13:01:32 +0200

    add generic modal component

--- app/_components/AddNoteModal.tsx
+++ app/_components/AddNoteModal.tsx
@@ -2,6 +2,7 @@
 
 import { PlusIcon } from "lucide-react";
 import { useState } from "react";
+import Modal from "./Modal";
 
 function AddNoteModal() {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,9 +12,16 @@ function AddNoteModal() {
       <button
         className="rounded-full bg-primary p-2 text-bg1 transition-colors duration-300 hover:bg-primary/60"
         aria-label="Open modal to add a note"
+        onClick={() => setIsOpen(true)}
       >
         <PlusIcon size={24} />
       </button>
+
+      {isOpen && (
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+          <div>meow</div>
+        </Modal>
+      )}
     </>
   );
 }
---
+++ app/_components/Modal.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import {
+  Dispatch,
+  ElementRef,
+  MouseEvent,
+  ReactNode,
+  SetStateAction,
+  SyntheticEvent,
+  useEffect,
+  useRef,
+} from "react";
+import { createPortal } from "react-dom";
+
+type Props = {
+  children: ReactNode;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+function Modal({ children, isOpen, setIsOpen }: Props) {
+  const dialogRef = useRef<ElementRef<"dialog">>(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      dialogRef.current?.showModal();
+    }
+  }, [isOpen]);
+
+  function onDismiss(event: SyntheticEvent<HTMLDialogElement, Event>) {
+    setIsOpen(false);
+  }
+
+  function handleCLickOutside(
+    event: MouseEvent<HTMLDialogElement, globalThis.MouseEvent>,
+  ) {
+    if (dialogRef.current && dialogRef.current === event.target) {
+      setIsOpen(false);
+    }
+  }
+
+  return (
+    isOpen &&
+    createPortal(
+      <dialog
+        ref={dialogRef}
+        onClose={onDismiss}
+        onClick={handleCLickOutside}
+        className="backdrop:backdrop-blur-sm"
+      >
+        {children}
+      </dialog>,
+      document.body,
+    )
+  );
+}
+
+export default Modal;
