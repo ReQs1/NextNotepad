3f06780e42803dfd3deea32f43e7c6f24e20820a
Req <snogedirl@gmail.com>
2025-01-29 10:54:34 +0100

    Add loading component for account routes

---
+++ app/account/loading.tsx
@@ -0,0 +1,11 @@
+import Spinner from "@/app/_components/Spinner";
+
+function Loading() {
+  return (
+    <main className="flex-1">
+      <Spinner />
+    </main>
+  );
+}
+
+export default Loading;
